1fa904524216dbdae2f8698487416f70317f150b
Ernest Mazur <emazemhs@gmail.com>
2021-10-03 02:57:08 +0200

    sec

--- src/Controller/Patterns/Builder/BuilderPatternController.php
+++ src/Controller/Patterns/Builder/BuilderPatternController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\Patterns\Builder\Auction;
-use App\Controller\Patterns\Builder\CAuction;
+use App\Controller\Patterns\Builder\AuctionC;
 
 class BuilderPatternController extends AbstractController
 {
---
+++ src/Controller/Patterns/Singleton/SingletonPatternController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Patterns\Singleton;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Patterns\Singleton\Bolder;
+
+class SingletonPatternController extends AbstractController
+{
+    /**
+     * @Route("/singleton-pattern", name="singleton-pattern")
+     */
+    public function index(): Response
+    {
+        $text = 'Wachalrz wspomnień';
+        dump($text);
+        // $bolder = new Bolder;
+        $bolder = Bolder::getInstance();
+        $text = $bolder->b($text);
+        dump($text);
+
+
+        return $this->render('patterns/builder.html.twig', [
+            'controller_name' => 'BuilderPatternController',
+        ]);
+    }
+}
